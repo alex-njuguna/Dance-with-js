fb33094a81325bfcc6deac47ecb963659b177279
= <=>
2023-07-06 14:31:21 +0300

    js new projects and updates

---
+++ questions/app.js
@@ -0,0 +1,28 @@
+// traversing the DOM
+/*
+const btns = document.querySelectorAll(".question-btn")
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement
+        question.classList.toggle('show-text')
+    })
+})
+*/
+//using selectors inside the element
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function (question){
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", function (){
+
+        questions.forEach(function (item){
+            if (item !== question){
+                item.classList.remove("show-text")
+            }
+        })
+
+        question.classList.toggle("show-text")
+    })
+})
